63b6ff3cd916a5c11f67e385dfa8ac6b948c0a91
CJ <cjllorente827@gmail.com>
2014-12-23 21:39:52 -0500

    Revert "Removed settings.py from repo"
    This reverts commit 161680a69e66003e7ca05380dd9ce45d3f229970.

---
+++ itt/settings.py
@@ -0,0 +1,16 @@
+"""
+Django settings for itt project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/dev/ref/settings/
+"""
+
+from dev_settings import *
+#from test_settings import *
+#from prod_settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '1ep!$(5*zvep*t9b^0icfw)2$@zazw$)sn205&=7#1_-#*yqi='
